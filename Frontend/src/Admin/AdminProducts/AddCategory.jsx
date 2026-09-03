dab1a4494c1be5aa1d33cb18afbadf07fd531865
qtechx001 <qtechx001@gmail.com>
2026-09-03 10:30:24 +0530

    add category fix

--- Frontend/src/Admin/AdminProducts/AddCategory.jsx
+++ Frontend/src/Admin/AdminProducts/AddCategory.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from 'react';
-import { Link, useNavigate, useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams, useLocation } from 'react-router-dom';
 import {
   ArrowLeft,
   CalendarDays,
